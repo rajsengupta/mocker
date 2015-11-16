271b7699b96fb80cb60411b7a0c36c8ee5ea3f6c
rsengupta@equalexperts.com <rsengupta@equalexperts.com>
2015-11-16 14:14:25 +0000

    [RS] adding a small example.

--- mocker.rb
+++ mocker.rb
@@ -1,4 +1,6 @@
 require 'mirage/client'
+require 'net/http'
+
 class Mocker
 
  def initialize
@@ -13,20 +15,37 @@ class Mocker
      default true
      delay delay
    end
+   @mirage_client.put('greeting', 'hello')
+ end
+
+ def service(uri,method,body='',headers={})
+
+   http = Net::HTTP.new(uri.host, uri.port)
+   if uri.host.include?('https')
+     http.use_ssl = true
+     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+   end
+   request = method.new(uri.request_uri)
+   request.body = body
+   # request.headers = {}
+   http.request(request)
  end
 
 
 end
 
-
-Mirage.start
 @mock_response = Mocker.new
+
 
 param_1='abc'
 param_2='acbe'
 make_time = {'productId'=>1, 'makeTime'=>2}
-@mock_response.set_response("/path/calling/api/#{param_1}/products/#{param_2}",make_time.to_json)
+path = "/path/calling/api/#{param_1}/products/#{param_2}"
+@mock_response.set_response(path,make_time.to_json)
+response1 = @mock_response.service(URI.parse("http://localhost:7001/responses/greeting"),Net::HTTP::Get)
+p response1.body
+response2 = @mock_response.service(URI.parse( "http://localhost:7001/responses#{path}"),Net::HTTP::Get)
+p response2.body
 
-
-# Mirage.stop :all
-# Mirage::Client.new.templates.delete_all
+Mirage::Client.new.templates.delete_all
+Mirage.stop :all
